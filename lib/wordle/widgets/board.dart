b06d44f9aa6995a66d85b11712f10026ac7f73c0
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-02-08 21:55:05 +0530

    Update board

--- lib/wordle/widgets/board.dart
+++ lib/wordle/widgets/board.dart
@@ -1,18 +1,46 @@
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:wordle_clone/wordle/wordle.dart';
 
-class Board extends StatefulWidget {
-  const Board({Key? key, required this.board}) : super(key: key);
+class Board extends StatelessWidget {
+  const Board({
+    Key? key,
+    required this.board,
+    required this.flipCardKeys,
+  }) : super(key: key);
+
   final List<Word> board;
+  final List<List<GlobalKey<FlipCardState>>> flipCardKeys;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: board.map((word) => Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: word.letters.map(letter) => BoardTile(letter: letter)
-      )
-      ).toList(),
+      children: board
+          .asMap()
+          .map((i, word) => MapEntry(
+              i,
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: word.letters
+                      .asMap()
+                      .map((j, letter) => MapEntry(
+                            j,
+                            FlipCard(
+                              key: flipCardKeys[i][j],
+                              flipOnTouch: false,
+                              direction: FlipDirection.VERTICAL,
+                              front: BoardTile(
+                                  letter: Letter(
+                                val: letter.val,
+                                status: LetterStatus.initial,
+                              )),
+                              back: BoardTile(letter: letter),
+                            ),
+                          ))
+                      .values
+                      .toList())))
+          .values
+          .toList(),
     );
   }
 }
